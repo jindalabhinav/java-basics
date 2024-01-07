56983db6505394ebc38bd695f9c48f37cee69a4f
jindalabhinav <jindal.abhinav@gmail.com>
2024-01-07 11:40:21 +0530

    Update Runner.java

--- src/Collection/Comparable/Runner.java
+++ src/Collection/Comparable/Runner.java
@@ -11,7 +11,6 @@ public class Runner {
         Pair<Integer> id2 = new Pair<>(1, 100);
         List<Pair<Integer>> ids = Arrays.asList(id1, id2);
 
-        List<Integer> integers = Arrays.asList(3, 2, 1);
         Collections.sort(ids);
         System.out.println(ids
                 .stream()
